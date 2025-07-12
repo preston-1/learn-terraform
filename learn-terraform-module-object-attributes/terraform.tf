fd24a1000a4f6404c72c89828210aca8522d340e
preston-1 <prestonglenn68@gmail.com>
2025-07-12 19:38:58 -0400

    - learn-private-module-root + learn modules changes

--- learn-terraform-module-object-attributes/terraform.tf
+++ learn-terraform-module-object-attributes/terraform.tf
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  cloud {
+  /*cloud {
     workspaces {
       name = "learn-terraform-module-object-attributes"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
